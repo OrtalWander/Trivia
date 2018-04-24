e34966f531300a4f88714b2f020e5ec8c1a9fc8e
Ortal W <o.parnassa@gmail.com>
2018-04-24 13:31:12 +0300

    question validation works!

--- app/controllers/questions.js
+++ app/controllers/questions.js
@@ -1,25 +1,22 @@
-myApp.controller('questions',function($scope,questionservice){
+myApp.controller('questions', function ($scope, questionservice) {
     $scope.questions = [];
-    questionservice.load().then(function(){
- $scope.questions = questionservice.questions;
+    questionservice.load().then(function () {
+        $scope.questions = questionservice.questions;
     })
-        $scope.count = 0;
-        $scope.myFunc = function() {
-          $scope.count++;
-        };
-//   step 1: save user's answer
-        function validation(userAnswer){
-        // step 2: compare to all questions in loop
-for(i=0;i<answers.length;i++){
-            // if correct- green frame
-if (userAnswer===$scope.questionservice.answers){
-    console.log("correctAnswer");
-}
-}
+    $scope.count = 0;
+    $scope.myFunc = function () {
+        $scope.count++;
+    };
+    //   step 1: save user's answer
+    $scope.validation = function(userAnswer, correctAnswer) {
+        if(userAnswer === correctAnswer){
+            alert ("Correct");
         }
+        
+    }
 
 
-        // in not correct- red frame and green on the correct answer
+    // in not correct- red frame and green on the correct answer
 
-        // count the correct answer
-    }); 
+    // count the correct answer
+});
